3515551b7df55ed1beee93c1297575243b332c02
lujs <327240969@qq.com>
2022-09-08 15:46:05 +0800

    feat: update doc

--- docs/docs/.umi-production/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/lujunsheng/myOwnX/clean-js/common/temp/node_modules/.pnpm/registry.npmmirror.com+@umijs+runtime@3.5.23_react@16.14.0/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu',],
-});
-
-export { plugin };
